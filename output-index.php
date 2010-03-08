6bc0e8dcefa5bf4879fc9b025618b08dbf296138
Frans de Jonge <Frenzie@users.noreply.github.com>
2010-03-08 10:09:35 +0100

    Fixed strange issue on Ubuntu with QUERY_STRING (seems always set)

--- output-index.php
+++ output-index.php
@@ -12,15 +12,6 @@ while (list($feedset, $feeds) = each($feedsets)) {
 		if (isset($feeds['sub_title'])) echo '<h3>'.$feeds['sub_title'].'</h3>';
 		echo '<a href="' . $feedset_uri . '">' . $feedset . '</a> (<a href="'.$folder_uri.'output-html.php?feed=' . $feedset_uri . '">view as HTML</a>)';
 		echo '<ul>';
-		/*foreach($feeds as $key => $feed) {
-			echo '<li>' . $feed;
-			if ($key == 'feeds') {
-				foreach ($feed as $feed) {
-					echo ' (<a href="'.$feed.'">original</a>, <a href="http://frenzie.dlinkddns.com/simplepie/mashup/output-html.php?feed=' . $feed . '">HTML</a>)';
-				}
-			}
-			echo'</li>';
-		}*/
 		foreach ($feeds['feeds'] as $feed) {
 			echo '<li>' . $feed;
 			echo ' (<a href="'.$feed.'">original</a>, <a href="http://frenzie.dlinkddns.com/simplepie/mashup/output-html.php?feed=' . $feed . '">HTML</a>)';
