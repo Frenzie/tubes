0c284deb06668b0cc4b9e43364303ac5d01c68b6
frenzie <frenzie@localhost>
2010-03-09 21:12:28 +0100

    Moved a few variabled out of the hardcoded part of the feed.

--- output-feed.php
+++ output-feed.php
@@ -14,23 +14,26 @@ $feed->set_stupidly_fast ( true ); // Remove or set to false if you don't trust 
 // Initialize the feed.
 $feed->init();
 
-// Make sure the page is being served with the UTF-8 headers.
-//$feed->handle_content_type();
-
+// Set the correct header for an atom feed.
 header('Content-Type: application/atom+xml;charset=utf-8');
 header('Vary: Accept');
 ?>
 <?php if ($feed->error): ?>
-		<p><?=$feed->error()?></p>
+ <p><?=$feed->error()?></p>
 <?php endif ?>
 <feed xmlns="http://www.w3.org/2005/Atom" xml:lang="en">
  <title><?php if (isset($feedset['title'])) echo $feedset['title']; else echo $feed_title; ?></title>
  <subtitle><?php if (isset($feedset['sub_title'])) echo $feedset['sub_title']; else echo $feed_sub_title; ?></subtitle>
- <author><name>Frans</name><uri>http://frans.lowter.us/</uri></author>
+ <author>
+  <name><?php if (isset($feedset['author_name'])) echo $feedset['author_name']; else echo $feed_author_name; ?></name>
+<?php if ( isset($feedset['author_url']) || isset($feed_author_url) ) { ?>
+	<uri><?php if (isset($feedset['author_url'])) echo $feedset; else echo $feed_author_url; ?></uri>
+<?php } ?>
+ </author>
  <link rel="self" type="application/atom+xml" href="<?php echo $feed_uri; ?>"/>
  <id><?php echo $feed_uri; ?></id>
-<?php if (isset($feed_icon)) { ?>
- <icon><?php echo $feed_icon; ?></icon>
+<?php if (isset($feedset['feed_icon'])) { ?>
+ <icon><?php echo $feedset['feed_icon']; ?></icon>
 <?php } ?>
  <updated><?php echo $feed->get_item()->get_date('Y-m-d\TH:i:sP');?></updated>
 <?php
@@ -56,7 +59,6 @@ header('Vary: Accept');
 	<link rel="alternate" type="text/html" href="<?php echo $item->get_permalink(); ?>"/>
 <!--	<summary></summary>-->
 	<content type="xhtml"><div xmlns="http://www.w3.org/1999/xhtml">
-	<?php //echo $item->get_content(); ?>
 	<?php echo $feed->fix_xhtml($item->get_content()); ?>
 	</div></content>
 	<published><?php echo $item->get_date('Y-m-d\TH:i:sP'); ?></published>
