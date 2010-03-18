93c4a4cc823ed1fa616cc0067656640d1ffcb89f
Frans de Jonge <Frenzie@users.noreply.github.com>
2010-03-18 14:03:59 +0100

    Preliminary better summary and content output.

--- output-feed.php
+++ output-feed.php
@@ -56,12 +56,18 @@ header('Vary: Accept');
 	</author>
 <?php } ?>
 	<title><?php echo strip_tags($item->get_title()); ?></title>
+<?php if ( $item->get_description() ) { ?>
 	<summary type="xhtml"><div xmlns="http://www.w3.org/1999/xhtml">
 	<?php echo $feed->fix_xhtml($item->get_description()); ?>
 	</div></summary>
+<?php } ?>
+<?php if ( $item->get_content() ) { ?>
 	<content type="xhtml"><div xmlns="http://www.w3.org/1999/xhtml">
 	<?php echo $feed->fix_xhtml($item->get_content()); ?>
 	</div></content>
+<?php }
+else echo '<content> </content>'; // The Atom spec requires some textual content, which is what the single space provides. This seems to be the single disadvantage compared to RSS. Perhaps replace with some custom code searching for something that could be proper to insert here like iTunes junk (SP is supposed to do that, but it doesn't seem to).
+?>
 	<published><?php echo $item->get_date('Y-m-d\TH:i:sP'); ?></published>
 	<updated><?php echo $item->get_date('Y-m-d\TH:i:sP'); ?></updated>
 	<id><?php echo $item->get_permalink(); ?></id>
